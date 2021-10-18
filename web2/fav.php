ed7ba6029d427055de2984c5607199d691684aec
MarkCPT21 <201980201@psu.palawan.edu.ph>
2021-10-18 09:52:34 +0800

    done Practice 5-01

--- web2/fav.php
+++ web2/fav.php
@@ -1,8 +1,19 @@
 <html>
 <head><title>Your Favorite</title></head>
 <body>
-    <?php
-        
-    ?>
+<?php
+    if(isset($_POST['username']) && $_POST['username'] != ''){
+        echo '<h3>Thanks for your selection '.$_POST['username'].'</h3>';
+        if(isset($_POST['color']) && isset($_POST['dish']) && $_POST['dish'] != ''){
+            echo '<h3>You really enjoy the '. $_POST['dish'].' dish <br> -  and especially with a nice '.$_POST['color'].' wine.</h3>';
+        }else{
+            echo '<h3>But please specify both your preferred dish and wine color '.$_POST['username'].'</h3>';
+        }
+    }else{
+        echo '<h1>Please Enter Your Username First!</h1>';
+    }
+
+    
+?>
 </body>
 </html>
