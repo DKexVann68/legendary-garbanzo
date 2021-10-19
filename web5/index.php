478b6a3fc9480ed0ec00846ab7fe16ac4504e533
MarkCPT21 <201980201@psu.palawan.edu.ph>
2021-10-19 17:10:28 +0800

    done practice 5-04 and practice 5-05

--- web5/index.php
+++ web5/index.php
@@ -1,7 +1,39 @@
 <?php
+/*
+PRACTICE SET 5-04
+What function do we use to start a session?
+- session_start();
+Where are these session variables stored? 
+- Unlike cookies session data is not stored in the 
+user's computer it is stored in the SERVER
+
+
+*/
+
+//PRACTICE SET 5-05
+session_start();
+
+if(isset($_SESSION['views'])){
+    $_SESSION['views'] = $_SESSION['views']+1;
+    $count = $_SESSION['views'];
+    if($_SESSION['views'] == 5){
+        echo 'Thank you for visiting the page a lot.<br>';
+    }
+    if($_SESSION['views'] == 10){
+        header('Location: congratulations.php');
+    }
+
+}else{
+    $_SESSION['views']=0;
+    $count = $_SESSION['views'];
+}
+      
 
 ?>
 
 <h1>Hello</h1>
-<p>You have visited this page <?= $num_visits ?> times</p>
-<p><a href="?reset=true">Reset</a></p>
+<p>You have visited this page <?php echo $count; ?> times</p>
+<form action="<?php $_SESSION['views']=0;  ?>">
+    <input type="submit" value="Reset">
+</form>
+
