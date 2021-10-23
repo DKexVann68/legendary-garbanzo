12c0d45c274475096326f41d17a634c2a990fc94
herlyn-ux <herlynpassionnelmida@gmail.com>
2021-10-23 10:50:36 +0800

    Done Practice set 5-06

---
+++ logApp-scaling-octo-rubyjets/config/config.php
@@ -0,0 +1,9 @@
+<?php
+	define('ROOT_URL', 'index.php');
+	define('DB_HOST', '127.0.0.1:3325');
+	define('DB_USER', 'root');
+	define('DB_PASS', '');
+	define('DB_NAME', 'user');
+
+
+?>
---
+++ logApp-scaling-octo-rubyjets/inc/footer.php
@@ -0,0 +1,9 @@
+<footer class="mastfoot mt-auto" style="text-align: center;">
+    <div class="inner">
+      <P><a href="https://github.com/herlyn-ux"></a>
+      <a href="https://github.com/MarkCPT21"></a>
+      <p>Created by <a href="https://github.com/KExVann">kjmartinez</a> for BSCS 3B1</p>
+    </div>
+</footer>
+  </body>
+</html>
