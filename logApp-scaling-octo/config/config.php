be48c4592c25807636989018a9d889f9b8a0897f
herlyn-ux <herlynpassionnelmida@gmail.com>
2021-11-26 00:02:49 +0800

    added some text

--- logApp-scaling-octo/config/config.php
+++ logApp-scaling-octo/config/config.php
@@ -1,5 +1,5 @@
 <?php
-	define('ROOT_URL', 'http://localhost/legendary-garbanzo/logApp-scaling-octo-rubyjets/index.php');
+	define('ROOT_URL', 'http://localhost/legendary-garbanzo/logApp-scaling-octo/index.php');
 	define('DB_HOST', '127.0.0.1:3325');
 	define('DB_USER', 'root');
 	define('DB_PASS', '');
